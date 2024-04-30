74cbefe1f45765ac3f5020a07a748b392c215f69
JJorgeP <74981962+JJorgeP@users.noreply.github.com>
2024-04-30 11:01:18 -0300

    Ajuste de imports

--- src/main/java/com/example/financeiro/api/resource/PessoaResource.java
+++ src/main/java/com/example/financeiro/api/resource/PessoaResource.java
@@ -1,7 +1,5 @@
 package com.example.financeiro.api.resource;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
@@ -12,7 +10,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-//import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
